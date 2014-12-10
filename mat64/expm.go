62d756b942a0d66514b96ff08f4da98b7652cfd3
Chatrapathi Akula <c2-akula@users.noreply.github.com>
2014-12-10 23:25:07 +0000

    new exponential matrix code
    concurrent and serial implementation added just for feedback. Both can be used however, checked it by using it in my robotics kinematics package. Performance numbers need to investigated further.

--- mat64/expm.go
+++ mat64/expm.go
@@ -1,166 +1,171 @@
 package mat64
 
-import "math"
+import (
+	"math"
+)
 
-// Ref: https://github.com/poliu2s/MKL/blob/master/matrix_exponential.cpp
-// Computes the exponential of an nxn matrix concurrently
-// Could make such that the receiver is the result
-// But this allows chaining so whichever works out neat
-func (A *Dense) ExpMC() *Dense {
-	nrows, ncols := A.Dims()
-	nelem := A.dataQuan()
-	const (
-		acc = 10.0
+// array to cache the factorials upto 20!
+var facts = make([]float64, 20)
 
-		// Scaling
-		N = 0.0
-	)
+// calculate the factorials and cache it
+func factorialMemoized(n float64) float64 {
+	if facts[int(n)] != 0 {
+		res := facts[int(n)]
+		return res
+	}
+
+	if n > 0 {
+		res := n * factorialMemoized(n-1)
+		return res
+	}
+	return 1.0
+}
+
+// Taylor Series Optimum Value Pair
+type taylor struct {
+	k, j float64
+}
+
+// www.cs.cornell.edu/cv/researchpdf/19ways+.pdf
+// https://github.com/poliu2s/MKL/blob/master/matrix_exponential.cpp
+// ExpMC computes the exponential of an nxn matrix concurrently using
+// Taylor Series + Scaling and Squaring Method
+// a and id should be same size
+func (m *Dense) ExpMC(a, id *Dense) {
+	// Use as many CPUs as available (good?)
+	// runtime.GOMAXPROCS(runtime.NumCPU())
+	ar, ac := a.Dims()
+	idr, idc := id.Dims()
+	if ar != idr && ac != idc {
+		panic(ErrShape)
+	}
+
+	// hardcoded optimum value pairs
+	// may not be needed but keeps it clean
+	// incase some update to the code is added
+	// just use a switch statement for different vals
+	// taylor.j can be taken as 0.0
+	tay := taylor{10, 1.0}
 
 	var (
-		msmall = new(Dense)
-		mpwr   = zeros(nrows, ncols)
-		mpwr1  = zeros(nrows, ncols)
-		mexp1  = zeros(nrows, ncols)
-		mexp2  = zeros(nrows, ncols)
-		temp   = zeros(nrows, ncols)
-		res    = eye(nrows)
-
-		mpwrD  = mpwr.data()
-		mpwr1D = mpwr1.data()
-		mexp1D = mexp1.data()
-		mexp2D = mexp2.data()
-		tempD  = temp.data()
-		resD   = res.data()
-
-		emchan  = make(chan *Dense, 1)
-		sqmchan = make(chan *Dense, 1)
+		// A divided by 2 power j (A / 2^j)
+		Ascl = new(Dense)
+		// Aj = A/2^j
+		Aj = new(Dense)
+		// jth power of A (A^j) divided by j factorial(j!)
+		Ajj = new(Dense)
+		// temporary Aj
+		Ajt = new(Dense)
+		// result matrix
+		Ar = new(Dense)
 	)
 
-	// M*(1/(2^N))
-	msmall.ScaleDense(1.0/math.Pow(2.0, N), M)
-	msmallD := msmall.data()
-	copy(mpwrD, msmallD)
+	Ascl.Scale(1/math.Pow(2, tay.j), a)
 
-	// Exponentiation Part
+	Aj.Clone(Ascl)
+	AjD := Aj.RawMatrix().Data
+
+	Ajj.Clone(id)
+	AjjD := Ajj.RawMatrix().Data
+
+	// initialize result matrix with Eye()
+	Ar.Clone(id)
+
+	emChan := make(chan *Dense, 1)
+
+	// Exponentiation
 	fact_i := 0.0
-	exp := func(i float64) {
-		fact_i = FactorialMemoized(i)
-		for i := 0; i < nelem; i++ {
-			tempD[i] = mpwrD[i] / fact_i
-		}
-		copy(mpwr1D, mpwrD)
-		res.Add(temp, res)
+	exp := func() {
+		go func() {
+			for j := 1.0; j < tay.k; j++ {
+				fact_i = factorialMemoized(j)
 
-		mpwr.Mul(mpwr1, msmall)
-		emchan <- res
-	}
-
-	// Squaring Part
-	sqm := func(r *Dense) {
-		for i := 0; i < N; i++ {
-			copy(mexp1D, resD)
-			copy(mexp2D, resD)
-			r.Mul(mexp1, mexp2)
-		}
-		sqmchan <- r
-	}
-
-	for i := 1.0; i < acc; i++ {
-		go exp(i)
-	}
-
-	select {
-	case r := <-emchan:
-		{
-			go sqm(r)
-			select {
-			case retm := <-sqmchan:
-				{
-					return retm
+				for i := 0; i < ar*ac; i++ {
+					AjjD[i] = AjD[i] / fact_i
 				}
+
+				Ajt.Clone(Aj)
+
+				// I + Mjj
+				Ar.Add(Ajj, Ar)
+
+				Aj.Mul(Ajt, Ascl)
 			}
+			emChan <- Ar
+		}()
+	}
+
+	// need further investigation into the performance difference
+	// since they produce the same result except go exp() runs independently
+	// while the other one stalls until it is finished
+	go exp()
+	// exp()
+
+	if r, ok := <-emChan; ok {
+		// squaring
+		for i := 0.0; i < tay.j; i++ {
+			m.Clone(r)
+			m.Mul(m, r)
 		}
 	}
-	return nil
 }
 
-// Computes the exponential of an nxn matrix serially
-// Again could make the receiver the result
-func (A *Dense) ExpMS() *Dense {
-	nrows, ncols := A.Dims()
-	nelem := A.dataQuan()
-	const (
-		acc = 10.0
+// Same as ExpMC
+// Serial Implementation
+func (m *Dense) ExpMS(a, id *Dense) {
+	ar, ac := a.Dims()
+	idr, idc := id.Dims()
+	if ar != idr && ac != idc {
+		panic(ErrShape)
+	}
 
-		// Scaling
-		N = 0.0
-	)
+	tay := taylor{10, 1.0}
 
 	var (
-		msmall = new(Dense)
-		mpwr   = zeros(nrows, ncols)
-		mpwr1  = zeros(nrows, ncols)
-		mexp1  = zeros(nrows, ncols)
-		mexp2  = zeros(nrows, ncols)
-		temp   = zeros(nrows, ncols)
-		res    = eye(nrows)
-
-		mpwrD  = mpwr.data()
-		mpwr1D = mpwr1.data()
-		mexp1D = mexp1.data()
-		mexp2D = mexp2.data()
-		tempD  = temp.data()
-		resD   = res.data()
+		// A divided by 2 power j (A / 2^j)
+		Ascl = new(Dense)
+		// Aj = A/2^j
+		Aj = new(Dense)
+		// jth power of A (A^j) divided by j factorial(j!)
+		Ajj = new(Dense)
+		// temporary Aj
+		Ajt = new(Dense)
+		// result matrix
+		Ar = new(Dense)
 	)
 
-	// M*(1/(2^N))
-	msmall.ScaleDense(1.0/math.Pow(2.0, N), M)
-	msmallD := msmall.data()
-	copy(mpwrD, msmallD)
+	// A/(2^j)
+	Ascl.Scale(1/math.Pow(2, tay.j), a)
 
+	Aj.Clone(Ascl)
+	AjD := Aj.RawMatrix().Data
+
+	Ajj.Clone(id)
+	AjjD := Ajj.RawMatrix().Data
+
+	// initialize result matrix with Eye()
+	Ar.Clone(id)
+
+	// Exponentiation
 	fact_i := 0.0
-	for i := 1.0; i < acc; i++ {
-		// Exponentiation Part
-		fact_i = FactorialMemoized(i)
-		for i := 0; i < nelem; i++ {
-			tempD[i] = mpwrD[i] / fact_i
+	for j := 1.0; j < tay.k; j++ {
+		fact_i = factorialMemoized(j)
+
+		for i := 0; i < ar*ac; i++ {
+			AjjD[i] = AjD[i] / fact_i
 		}
-		copy(mpwr1D, mpwrD)
-		res.Add(temp, res)
 
-		mpwr.Mul(mpwr1, msmall)
+		Ajt.Clone(Aj)
+
+		// I + Ajj
+		Ar.Add(Ajj, Ar)
+
+		Aj.Mul(Ajt, Ascl)
 	}
 
-	for i := 0; i < N; i++ {
-		// Squaring Part
-		copy(mexp1D, resD)
-		copy(mexp2D, resD)
-		res.Mul(mexp1, mexp2)
+	// Squaring
+	for i := 0.0; i < tay.j; i++ {
+		m.Clone(Ar)
+		m.Mul(m, Ar)
 	}
-	return res
-}
-
-// Create a zero matrix
-func zeros(r, c int) *Dense {
-	return NewDense(r, c, nil)
-}
-
-// Create an identity matrix
-func eye(span int) *Dense {
-	A := NewDense(span, span, nil)
-	for i := 0; i < span; i++ {
-		A.Set(i, i, 1)
-	}
-	return A
-}
-
-// Gives the underlying elements array
-func (A *Dense) data() []float64 {
-	RawM := A.RawMatrix()
-	return len(RawM)
-}
-
-// gives number of elements in matrix
-func (A *Dense) dataQuan() int {
-	return len(A.data())
 }
