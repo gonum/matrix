13bedbb4667b2f7148efa05310cd9fa675730e1b
kortschak <dan.kortschak@adelaide.edu.au>
2015-01-14 14:46:19 +1030

    Revise Eigen design
    This is a minimally invasive change that exposes the eigenvalues while
    retaining the previous API.

--- mat64/eigen_test.go
+++ mat64/eigen_test.go
@@ -16,8 +16,8 @@ func (s *S) TestEigen(c *check.C) {
 
 		epsilon float64
 
-		e, d []float64
-		v    *Dense
+		ev EigenValues
+		v  *Dense
 	}{
 		{
 			a: NewDense(3, 3, []float64{
@@ -28,8 +28,7 @@ func (s *S) TestEigen(c *check.C) {
 
 			epsilon: math.Pow(2, -52.0),
 
-			d: []float64{3.0000000000000044, -4.000000000000003, -1.0980273383714707e-16},
-			e: []float64{0, 0, 0},
+			ev: []complex128{3.0000000000000044, -4.000000000000003, -1.0980273383714707e-16},
 			v: NewDense(3, 3, []float64{
 				-0.48507125007266627, 0.41649656391752204, 0.11785113019775795,
 				-0.7276068751089995, -0.8329931278350428, 0.7071067811865481,
@@ -45,8 +44,7 @@ func (s *S) TestEigen(c *check.C) {
 
 			epsilon: math.Pow(2, -52.0),
 
-			d: []float64{-6.240753470718579, -1.3995889142010132, 6.640342384919599},
-			e: []float64{0, 0, 0},
+			ev: []complex128{-6.240753470718579, -1.3995889142010132, 6.640342384919599},
 			v: NewDense(3, 3, []float64{
 				-0.6134279348516111, -0.31411097261113, -0.7245967607083111,
 				0.7697297716508223, -0.03251534945303795, -0.6375412384185983,
@@ -60,6 +58,8 @@ func (s *S) TestEigen(c *check.C) {
 				1, 3, 6,
 			}),
 
+			ev: []complex128{0.34508918353562557, 3.5955906738074535, 8.059320142656922},
+
 			epsilon: math.Pow(2, -52.0),
 		},
 		{ // Jama evals
@@ -69,6 +69,8 @@ func (s *S) TestEigen(c *check.C) {
 				0, -2e-7, 0, 1,
 				0, 0, 1, 0,
 			}),
+
+			ev: []complex128{-0.9999999999999909 + 9.999999966061991e-08i, -0.9999999999999909 - 9.999999966061991e-08i, 0.9999999999999932 + 9.999999990731938e-08i, 0.9999999999999932 - 9.999999990731938e-08i},
 
 			epsilon: math.Pow(2, -52.0),
 		},
@@ -81,23 +83,33 @@ func (s *S) TestEigen(c *check.C) {
 				1, 0, 1, 0, 1,
 			}),
 
+			ev: []complex128{1.6180339887498956, 1.1102230246251565e-16, -0.6180339887498951, 0.9999999999999997i, -0.9999999999999997i},
+
+			epsilon: math.Pow(2, -52.0),
+		},
+		{
+			a: NewDense(3, 3, []float64{
+				1, 6, 7,
+				9, 1, 10,
+				15, 6, 1,
+			}),
+
+			ev: []complex128{18.13437754618839, -7.567188773094204 + 1.0909493212308445i, -7.567188773094204 - 1.0909493212308445i},
+
 			epsilon: math.Pow(2, -52.0),
 		},
 	} {
 		ef := Eigen(DenseCopyOf(t.a), t.epsilon)
-		if t.d != nil {
-			c.Check(ef.d, check.DeepEquals, t.d)
-		}
-		if t.e != nil {
-			c.Check(ef.e, check.DeepEquals, t.e)
+		if t.ev != nil {
+			c.Check(ef.Values, check.DeepEquals, t.ev)
 		}
 
 		if t.v != nil {
-			c.Check(ef.V.Equals(t.v), check.Equals, true)
+			c.Check(ef.Vectors.Equals(t.v), check.Equals, true)
 		}
 
-		t.a.Mul(t.a, ef.V)
-		ef.V.Mul(ef.V, ef.D())
-		c.Check(t.a.EqualsApprox(ef.V, 1e-12), check.Equals, true)
+		t.a.Mul(t.a, ef.Vectors)
+		ef.Vectors.Mul(ef.Vectors, ef.Values.D())
+		c.Check(t.a.EqualsApprox(ef.Vectors, 1e-12), check.Equals, true)
 	}
 }
